77a3643b3175fbe81a370be82fd53556d6510ea9
Charlie Strawn <cdstrawn@gmail.com>
2021-01-06 08:51:51 -0600

    add hello world rest endpoint

--- src/main/java/com/charliestrawn/learn/controllers/RootController.java
+++ src/main/java/com/charliestrawn/learn/controllers/RootController.java
@@ -1,10 +1,14 @@
 package com.charliestrawn.learn.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RootController {
 
-    
+    @GetMapping("/greeting")
+    public String getRoot(){
+        return "hello world";
+    }
 
 }
